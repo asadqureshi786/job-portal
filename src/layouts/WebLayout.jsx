db4dfd101eae7abbb24091b21d5e1427d6e9d17f
Asad Qureshi <asad.next.action@gmail.com>
2026-07-07 23:43:52 +0500

    working on register page

--- src/layouts/WebLayout.jsx
+++ src/layouts/WebLayout.jsx
@@ -15,7 +15,6 @@ export default function WebLayout() {
       <div className="main_wrapper website lg:mt-24 mt-19.5 [&_p]:text-gray-600 dark:[&_p]:text-gray-200 dark:bg-black/30 transition-all">
         <Outlet />
       </div>
-
       <Footer />
       <ThemeBox/>
       
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -10,8 +10,22 @@ export default function Register() {
         </div>
         <div className="col-span-6 flex justify-center items-center flex-col ">
           <div className='text-center '>
-            <h2 className='text-3xl font-bold text-gray-800'>Sign Up Account</h2>
-            <p className='text-sm mt-3' >Enter your details to create an account</p>
+            <h2 className='text-4xl font-bold text-gray-800 dark:text-white'>Sign Up Account</h2>
+            <p className='text-sm mt-3 dark:text-white' >Enter your details to create an account</p>
+          </div>
+          <div className='flex gap-4 mt-6'>
+            <label>
+              <input type="radio" className='hidden peer' name="account-type" defaultChecked  />
+              <div className='text-[15px] transition-all border border-priamry bg-transparent text-primary   rounded-lg flex items-center justify-center px-5 py-1.5 cursor-pointer peer-checked:bg-primary peer-checked:text-white dark:peer-checked:border-primary' >
+                Job Seeker
+              </div>
+            </label>
+            <label>
+              <input type="radio" className='hidden peer' name="account-type" />
+              <div className='text-[15px] transition-all border border-primary bg-transparent text-primary   rounded-lg flex items-center justify-center px-5 py-1.5 cursor-pointer peer-checked:bg-primary peer-checked:text-white dark:peer-checked:border-primary' >
+                Job Poster
+              </div>
+            </label>
           </div>
         </div>
     </div>
