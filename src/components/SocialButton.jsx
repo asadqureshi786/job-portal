50486ca4a468716f3cefa860e2142c1eeadab4ae
asadqureshi786 <98189738+asadqureshi786@users.noreply.github.com>
2026-07-21 15:35:15 +0500

    wrap social button in components

---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function SocialButton() {
+    return (
+        <div className="grid grid-cols-2 items-center gap-2">
+            <button className="w-full flex justify-center items-center gap-3 text-base gray-300 rounded-md border border-gray-200 dark:border-gray-900 dark:text-slate-300 py-1.5 px-5 transition-all hover:bg-gray-100 cursor-pointer">
+                <img src="/img/c_logo/google.png" className="w-4.5" />
+                Google</button>
+            <button className="full  flex justify-center items-center gap-3 text-base gray-300 rounded-md border border-gray-200 dark:border-gray-900 dark:text-slate-300 py-1.5 px-5 transition-all hover:bg-gray-100 cursor-pointer">
+                <img src="/img/c_logo/apple.png" className="w-4.5 bloack dark:hidden!" />
+                <img src="/img/c_logo/apple-darl.png" className="w-4.5 hidden dark:block!" />
+                Google</button>
+        </div>
+    )
+}
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import ThemeBox from "../../components/ThemeBox";
-
+import SocialButton from "../../components/SocialButton";
 export default function Login() {
 
  
@@ -44,15 +44,11 @@ export default function Login() {
               <p className="text-center text-gray-500 font-thin font-Anton mb-0 py-6 text-sm cursor-pointer">
                 Or 
               </p>
-              <div className="grid grid-cols-2 items-center gap-2">
-                <button className="w-full flex justify-center items-center gap-3 text-base gray-300 rounded-md border border-gray-200 dark:border-gray-900 dark:text-slate-300 py-1.5 px-5 transition-all hover:bg-gray-100 cursor-pointer">
-                  <img src="/img/c_logo/google.png" className="w-4.5" />
-                  Google</button>
-                <button className="full  flex justify-center items-center gap-3 text-base gray-300 rounded-md border border-gray-200 dark:border-gray-900 dark:text-slate-300 py-1.5 px-5 transition-all hover:bg-gray-100 cursor-pointer">
-                  <img src="/img/c_logo/apple.png" className="w-4.5 bloack dark:hidden!" />
-                      <img src="/img/c_logo/apple-darl.png" className="w-4.5 hidden dark:block!" />
-                  Google</button>
-              </div>
+             
+
+                <SocialButton/>
+
+
               <p className="text-center text-gray-500 font-thin font-Anton mb-0 py-6 text-sm">
                 Don't Have An Account? <span className="text-primary font-medium" >Sign up</span>
               </p>
